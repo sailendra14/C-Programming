6c2991f11fd05d3174665ab4e92f2e097a72ccd5
sailendra14 <sailendraindia14@gmail.com>
2020-07-05 17:52:17 +0530

    vid 7

---
+++ operators.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    // Arithmetic Operators
+
+    // int a = 10;
+    // float  b = 4.90;
+
+    // printf("The sum is: %f \n ", a + b);
+    // printf("The Subtraction is: %f \n ", a - b);
+    // printf("The multiplication is: %f \n ", a * b);
+    // printf("The division is: %f \n", a / b);
+
+    //Relationsl Operators
+   
+    // int a = 4;
+    // int  b = 4;
+
+    // printf("a + b: %d \n ", a == b);
+    
+
+    return 0;
+} 
+
+// Always use %f if any one value is float, else use %d.
