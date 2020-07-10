261157afd9c594471477611a37c303e440abc8d4
sailendra14 <sailendraindia14@gmail.com>
2020-07-10 07:37:25 +0530

    vid 8

---
+++ multiplication_table.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{ 
+    int a,b;
+
+
+    if a = "%d"
+    //body
+    a X a = b; 
+    a X a = b; 
+    a X a = b; 
+    a X a = b; 
+    a X a = b; 
+    a X a = b; 
+    a X a = b; 
+    a X a = b; 
+    a X a = b; 
+    a X a = b; 
+    a X a = b; 
+    a X a = b; 
+    return 0;
+}
